eac062a63f80ca3ca3bc4644ffa29555ab9c46a5
Zach Eilon <zeilon@ldeo.columbia.edu>
2017-12-04 16:25:52 -0800

    update to build prior

--- a2_BUILD_PRIOR.m
+++ a2_BUILD_PRIOR.m
@@ -1,4 +1,5 @@
-function prior = a2_BUILD_PRIOR(par,Niter,zmantle)
+function prior = a2_BUILD_PRIOR(par,Niter,zatdep)
+% prior = a2_BUILD_PRIOR(par,Niter,zatdep)
 % profile clear
 % profile on
 
@@ -12,13 +13,13 @@ prior = struct('Niter',Niter','Npass',0,...
                'zsed',o,'zmoh',o,...
                'kcrust',o,'kmantle',o,...
                'VSsedtop',o,'VSsedbot',o,'VScrusttop',o,'VScrustbot',o,...
-               'VSmantle',repmat(o,1,6),'zmantle',zeros(6,1),...
+               'VSmantle',repmat(o,1,6),'zatdep',zeros(6,1),...
                'fdVSsed',o,'fdVSmoh',o,'vpvs',o);
 
-if nargin<3 || isempty(zmantle)
-    prior.zmantle = linspace(par.mod.sed.hmax+par.mod.crust.hmin+0.1,par.mod.maxz,50)';
+if nargin<3 || isempty(zatdep)
+    prior.zatdep = linspace(par.mod.sed.hmax+par.mod.crust.hmin+0.1,par.mod.maxz,50)';
 else
-    prior.zmantle = zmantle;
+    prior.zatdep = zatdep;
 end
 
 % prep for par
@@ -37,11 +38,13 @@ for kk = 1:Niter
     
     parprior(kk).zsed = model.zsed; %#ok<PFOUS>
     parprior(kk).zmoh = model.zmoh;
+    parprior(kk).kcrust = model.crustmparm.Nkn;
+    parprior(kk).kmantle = model.mantmparm.Nkn;
     parprior(kk).VSsedtop = model.sedmparm.VS(1);
     parprior(kk).VSsedbot = model.sedmparm.VS(2);
     parprior(kk).VScrusttop = model.crustmparm.VS_sp(1);
     parprior(kk).VScrustbot = model.crustmparm.VS_sp(end);
-    parprior(kk).VSmantle(1,:) = linterp(model.z,model.VS,prior.zmantle);
+    parprior(kk).VSmantle(1,:) = linterp(model.z,model.VS,prior.zatdep);
     parprior(kk).fdVSsed = model.fdVSsed;
     parprior(kk).fdVSmoh = model.fdVSmoh;
     parprior(kk).vpvs = model.crustmparm.vpvs;    
@@ -61,13 +64,14 @@ prior.VSsedtop = [parprior.VSsedtop]';
 prior.VSsedbot = [parprior.VSsedbot]';
 prior.VScrusttop = [parprior.VScrusttop]';
 prior.VScrustbot = [parprior.VScrustbot]';
-prior.VSmantle = reshape([parprior.VSmantle],length(prior.zmantle),Npass)';
+prior.VSmantle = reshape([parprior.VSmantle],length(prior.zatdep),Npass)';
 prior.fdVSsed = [parprior.fdVSsed]';
 prior.fdVSmoh = [parprior.fdVSmoh]';
 prior.vpvs = [parprior.vpvs]';
 prior.cxi = [parprior.cxi]';
 prior.mxi = [parprior.mxi]';
-prior.zmantle = round(prior.zmantle);
+prior.zatdep = round(prior.zatdep);
+prior.zmantle = prior.zatdep;
 
 
 
