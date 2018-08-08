c5e36f4c8f3c2ef82655e887695b376c684c544f
eilonzach <eilon@ucsb.edu>
2018-08-08 12:39:22 -0700

    lots of additions and re-arrangements within MASTER_par script, including saving data periodically, and how the kernel updating is done

--- MASTER_par.m
+++ MASTER_par.m
@@ -11,7 +11,7 @@ gc = [69,59,40,38,36,66]; % will search for gcarcs +/-3 of this value;
 notes = [...
     'Synthetic test using spline-based model. '...
     'Using all data types. '...
-    'Changed the way the spline kernels are re-calculated after N iterations, so as to hopefully avoid big jumps in likelihood and therefore hanging chains. '...
+    'Changed the way the spline kernels are re-calculated after N iterations, so as to prevent big jumps in likelihood and therefore hanging chains. '...
     'Several previous runs have shut down computer for reasons I do not understand. '...
     'Execute for few iterations to try to see what breaks, but in a short run time... '...
 ]; 
@@ -48,7 +48,8 @@ elseif strcmp(projname,'LAB_tests')
 	zlab = 130;
 	wlab = 10;
 	flab = 0.05;
-	dtps = {'BW_Ps_cms','SW_Ray_phV'};     
+    par.synth.model = struct('zsed',zsed,'zmoh',zmoh,'zlab',zlab,'wlab',wlab,'flab',flab);
+	dtps = {'BW_Ps','BW_Sp','BW_Sp_lo','BW_Ps_lo','SW_Ray_phV','SW_Lov_phV','SW_HV'};     
 
 	% noise details, if "real"
 	noisesta = 'RSSD';
@@ -60,9 +61,13 @@ elseif strcmp(projname,'LAB_tests')
 	% naming convention
 	dtpstr='_';
 	if any(strcmp(dtps,'BW_Ps')), dtpstr=[dtpstr,'Ps']; end
+	if any(strcmp(dtps,'BW_Ps_lo')), dtpstr=[dtpstr,'Pslo']; end
 	if any(strcmp(dtps,'BW_Ps_cms')), dtpstr=[dtpstr,'Pscms']; end
 	if any(strcmp(dtps,'BW_Sp')), dtpstr=[dtpstr,'Sp']; end
-	if any(strcmp(dtps,'SW_Ray_phV')), dtpstr=[dtpstr,'SW']; end
+	if any(strcmp(dtps,'BW_Sp_lo')), dtpstr=[dtpstr,'Splo']; end
+	if any(strcmp(dtps,'SW_Ray_phV')), dtpstr=[dtpstr,'Ray']; end
+	if any(strcmp(dtps,'SW_Lov_phV')), dtpstr=[dtpstr,'Lov']; end
+	if any(strcmp(dtps,'SW_HV')), dtpstr=[dtpstr,'HV']; end
 
 	ifsavedat = true;
 
@@ -74,6 +79,10 @@ else
     par.gc = gc;
     datN = 20;
 end
+
+par.synth.noise_sta_deets = struct('datadir',['/Volumes/data/THBI/US/STAsinv/',noisesta,'_dat20/'],...
+                         'sta',noisesta,'nwk',noisenwk,'gc',noisegcarcs,'noiseup',noiseup,'noiseshape',noiseshape);
+
 
 par.inv.verbose=false;
 
@@ -94,106 +103,13 @@ for id = 1:length(par.inv.datatypes)
 end
 
 %% ========================  LOAD + PREP DATA  ========================  
-fprintf('LOADING data\n')
-if strcmp(projname,'SYNTHETICS')
-    
-    fprintf(' > Creating custom model and synthetic data\n')
-    
-    % make synth model
-    z0_SYNTH_MODEL_fig3_splinemod(par,0);  %close(95)
-    save([resdir,'/trumodel'],'TRUEmodel');
-
-    % make synth data
-    [ trudata ] = z1_SYNTH_DATA(par,0); % in ZRT format
-    if strcmp(par.synth.noisetype,'real')
-        noise_sta_deets = struct('datadir',['/Volumes/data/THBI/US/STAsinv/',noisesta,'_dat20/'],...
-								 'sta',noisesta,'nwk',noisenwk,'gc',noisegcarcs,'noiseup',noiseup,'noiseshape',noiseshape);
-        par.synth.noise_sta_deets = noise_sta_deets;
-        [ trudata,par ] = z2_NOISIFY_SYNTH( trudata, par,noise_sta_deets );
-    end
-
-    % distribute data for different processing (e.g. _lo, _cms)
-    trudata = duplicate_data_distribute(trudata,par);
-    stadeets = struct('Latitude',[],'Longitude',[]);
-
-elseif strcmp(projname,'LAB_tests')
-	z0_SYNTH_MODEL_LAB_TEST(par,zsed,zmoh,zlab,wlab,flab,1) ;
-	save([resdir,'/trumodel'],'TRUEmodel');
-
-	% make synth data
-	[ trudata ] = z1_SYNTH_DATA(par,0); % in ZRT format
-	trudata_noiseless = trudata;
-
-	trudata = trudata_noiseless;
-	if strcmp(par.synth.noisetype,'real')
-		noise_sta_deets = struct('datadir',['/Volumes/data/THBI/US/STAsinv/',noisesta,'_dat20/'],...
-								 'sta',noisesta,'nwk',noisenwk,'gc',noisegcarcs,'noiseup',noiseup,'noiseshape',noiseshape);
-		par.synth.noise_sta_deets = noise_sta_deets;
-	%     [ trudata,par ] = z2_NOISIFY_SYNTH_makestack( trudata, par,noise_sta_deets );
-		[ trudata,par ] = z2_NOISIFY_SYNTH( trudata, par,noise_sta_deets );
-    end
-    
-    trudata = duplicate_data_distribute(trudata,par);
-    stadeets = struct('Latitude',[],'Longitude',[]);
-
-else
-	try stadeets = irisFetch.Stations('station',nwk,sta,'*','*'); 
-    catch, load([proj.rawdatadir,'/stainfo_master.mat']); 
-        stadeets = struct('Latitude',stainfo(strcmp({stainfo.StationCode},sta)).Latitude,...
-                          'Longitude',stainfo(strcmp({stainfo.StationCode},sta)).Longitude);
-    end
-
-%     [~,~,~,TRUEmodel.Z,TRUEmodel.vs,TRUEmodel.vp,TRUEmodel.rho] = RD_1D_Vprofile; close(gcf);
-    [trudata,zeroDstr] = load_data(avardir,sta,nwk,gc);
-    sta = [sta,zeroDstr];
-    % distribute data for different processing (e.g. _lo, _cms)
-    trudata = duplicate_data_distribute(trudata,par);
-    
-    for idt = 1:length(par.inv.datatypes)
-        dtype = par.inv.datatypes{idt}; pdt = parse_dtype( dtype ); 
-        if ~isfield(trudata,par.inv.datatypes{idt}) && strcmp(pdt{1},'BW') 
-            trudata.(dtype) = trudata.([pdt{1},'_',pdt{2}]); % insert standard BW if needed
-        end
-        % set prior sigma as geometric mean of data sigma
-        if isfield(trudata.(dtype),'sigma') && ~isnan(geomean(trudata.(dtype).sigma))
-            par.mod.data.prior_sigma.(pdt{1}).(pdt{2}).(pdt{3}) = geomean(trudata.(dtype).sigma);
-        end
-    end
-
-    % get rid of data that wont be used in inversion - NB NEED EXACT DATA MATCH
-    trudtypes = fieldnames(trudata);
-    for idt = 1:length(trudtypes)
-        if all(~strcmp(trudtypes{idt},par.inv.datatypes)) % no match
-            fprintf('WARNING - removing %s data from trudata\n',trudtypes{idt})
-            trudata = rmfield(trudata,trudtypes{idt});
-        end
-        if par.inv.BWclust~=0 && any(regexp(trudtypes{idt},'BW'))
-            fprintf('WARNING - removing %s data not in cluster %.0f\n',trudtypes{idt},par.inv.BWclust)
-            trudata.(trudtypes{idt}) = trudata.(trudtypes{idt})([trudata.(trudtypes{idt}).clust]==par.inv.BWclust);
-        end
-    end
-
-end
-
-% save data
-save([resdir,'/trudata_ORIG'],'trudata');
-
-% window, filter data 
-for idt = 1:length(par.inv.datatypes)
-    dtype = par.inv.datatypes{idt};
-    [ trudata ] = predat_process( trudata,dtype,par);
-end
-% plot_TRU_WAVEFORMS(trudata);
-% plot_TRUvsPRE(trudata,trudata);
-save([resdir,'/trudata_USE'],'trudata');
-
-C = parallel.pool.Constant(trudata)
+trudata = a2_LOAD_DATA(par,projname,resdir);
 
 
 %% ===========================  PRIOR  ===========================  
 zatdep = [5:5:par.mod.maxz]';
 % fprintf('  > Building prior distribution from %.0f runs\n',max([par.inv.niter,1e5]))
-% prior = a2_BUILD_EMPIRICAL_PRIOR(par,max([par.inv.niter,1e5]),14,zatdep);
+% prior = a3_BUILD_EMPIRICAL_PRIOR(par,max([par.inv.niter,1e5]),14,zatdep);
 % plot_MODEL_SUMMARY(prior,1,[resdir,'/prior_fig.pdf']);
 % save([resdir,'/prior'],'prior','par');
 
@@ -203,6 +119,9 @@ zatdep = [5:5:par.mod.maxz]';
 profile clear
 profile on
 delete(gcp('nocreate'));
+
+TD = parallel.pool.Constant(trudata);
+% TD(1).Value = trudata;
 
 %% START DIFFERENT MARKOV CHAINS IN PARALLEL
 % nchains = ;
@@ -222,10 +141,6 @@ t = now;
 parfor iii = 1:par.inv.nchains 
 chainstr = mkchainstr(iii);
 
-%% >>> testing
-% n_since_acc = 0;
-% accdec = struct('iter',[],'logL_current',[],'logL_proposed',[],'ifacc',[],'temp',[],'preF',[],'newk',[]);
-%% <<< testing
 
 %% Fail-safe to restart chain if there's a succession of failures
 fail_chain=20;
@@ -241,20 +156,21 @@ while ifpass==0
     while ifpass==0 % first make sure the starting model satisfies conditions
         model0 = b1_INITIATE_MODEL(par);
         model = model0;
-        model = expand_dathparms_to_data( model,trudata,par );
+        model = expand_dathparms_to_data( model,TD.Value,par );
         ifpass = a1_TEST_CONDITIONS( model, par );
     end
 
     %% starting model kernel
     fprintf('\nCreating starting kernels %s\n',chainstr)
     try
-        [Kbase] = make_allkernels(model,[],trudata,['start',chainstr],par);
+        [Kbase] = make_allkernels(model,[],TD.Value,['start',chainstr],par);
     catch
         ifpass = false; continue; 
     end
     
+    model0_perchain{iii} = model0;
+    
 end % now we have a starting model!
-
 
 %% ========================================================================
 %% ------------------------- Start iterations -----------------------------
@@ -263,8 +179,8 @@ ptb = cell({});
 nchain = 0;
 fail_chain = 0;
 ifaccept=true;
-if isfield(trudata,'SW_Ray')
-    preSW = zeros(length(trudata.SW_Ray.periods),ceil(par.inv.niter./par.inv.saveperN));
+if isfield(TD.Value,'SW_Ray')
+    preSW = zeros(length(TD.Value.SW_Ray.periods),ceil(par.inv.niter./par.inv.saveperN));
 end
 % reset_likelihood;
 log_likelihood = -Inf;
@@ -273,12 +189,17 @@ predata=[]; predat_save = []; misfit = [];
 fprintf('\n =========== STARTING ITERATIONS %s ===========\n',chainstr)
 for ii = 1:par.inv.niter
     
-%% SAVE every saveperN
+%% SAVE model every saveperN
 if mod(ii,par.inv.saveperN)==0 && log_likelihood ~= -Inf
     [misfits,allmodels,savedat] = b9_SAVE_RESULT(ii,log_likelihood,misfit,model,misfits,allmodels,predat_save,savedat);
-    if isfield(trudata,'SW_Ray')
+    if isfield(TD.Value,'SW_Ray')
         preSW(:,misfits.Nstored) = predata.SW_Ray.phV;
     end
+end
+
+%% SAVE inv state every 2500 iterations
+if rem(ii,2500)==0
+    save_inv_state(resdir,chainstr,allmodels,misfits)
 end
     
 try
@@ -319,7 +240,7 @@ try
 		if ~ifpass, if par.inv.verbose, fprintf('  nope\n'); end; break; end
 		
 		[ modptb ] = calc_Vperturbation( Kbase.modelk,model1);
-		ptbnorm = norm(modptb.dvsv) + norm(modptb.dvsh);
+		ptbnorm = 0.5*(norm(modptb.dvsv) + norm(modptb.dvsh)) + norm(modptb.dvpv);
     end
 
     if isempty(gcp('nocreate'))
@@ -345,8 +266,9 @@ try
         % make random run ID (to avoid overwrites in parfor)
 		ID = [chainstr,num2str(round(1e9*(now-t))),num2str(randi(9)),num2str(randi(9))];
 
-		try
-            predata = b3_FORWARD_MODEL( model1,Kbase,par,trudata,ID,0);
+        try
+            predata = b3_FORWARD_MODEL_BW( model1,par,TD.Value,ID,0 );
+            predata = b3_FORWARD_MODEL_SW_kernel( model1,Kbase,par,predata );
         catch
             fail_chain=fail_chain+1;
             fprintf('Forward model error, failchain %.0f\n',fail_chain);  break;
@@ -361,57 +283,28 @@ try
         predata0=predata;
         
         for idt = 1:length(par.inv.datatypes)
-            [ predata ] = predat_process( predata,par.inv.datatypes{idt},par);
+            predata = predat_process( predata,par.inv.datatypes{idt},par);
         end
         
 		% Explicitly use mineos + Tanimoto scripts if ptb is too large
 		if par.inv.verbose, fprintf('    Perturbation %.2f\n',ptbnorm); end
-		if ptbnorm/par.inv.kerneltolmax > random('unif',par.inv.kerneltolmed/par.inv.kerneltolmax,1,1) % control chance of going to MINEOS
+        if ptbnorm/par.inv.kerneltolmax > random('unif',par.inv.kerneltolmed/par.inv.kerneltolmax,1,1) % control chance of going to MINEOS
             newK = true;
         end
-        % ALSO use MINEOS if chain too long
-        if nchain > par.inv.maxnkchain
-            newK = true;
+%         % ALSO use MINEOS if chain too long
+        if nchain > 2*par.inv.maxnkchain
+            fprintf('Chain %s, iter %.0f, nchain=%.0f\n',chainstr,ii,nchain);
         end
-        
+            
             
         if newK
-            SW = struct('Ray',struct('phV',[],'grV',[]),'Lov',struct('phV',[],'grV',[]),'HV',struct('HVr',[]));
-            
-            if any(strcmp(allpdytp(:,2),'Ray')), itp = par.inv.datatypes(find(strcmp(allpdytp(:,2),'Ray'),1,'first')); %#ok<PFBNS>
-                [SW.Ray.phV,SW.Ray.grV] = run_mineos(model1,trudata.(itp{1}).periods,'R',ID,0,0,par.inv.verbose);
-            end
-            if any(strcmp(allpdytp(:,2),'Lov')), itp = par.inv.datatypes(find(strcmp(allpdytp(:,2),'Lov'),1,'first'));
-                [SW.Lov.phV,SW.Lov.grV] = run_mineos(model1,trudata.(itp{1}).periods,'L',ID,0,0,par.inv.verbose);
-            end
-            if any(strcmp(allpdytp(:,2),'HV')), itp = par.inv.datatypes(find(strcmp(allpdytp(:,2),'HV'),1,'first'));
-                [SW.HV.HVr,HVK_new] = run_HVkernel(model1,trudata.(itp{1}).periods,['HV_',ID],1,0,par.inv.verbose);
-            end
-            for id = 1:length(par.inv.datatypes)
-                
-                dtype = par.inv.datatypes{id}; pdtyp=parse_dtype(dtype); 
-                if ~strcmp(pdtyp{1},'SW'),continue; end
-                switch pdtyp{2}
-                    case {'Ray','Lov'}
-                        swk = predata.(dtype).phV; % record existing phV from kernels
-                        predata.(dtype).phV = SW.(pdtyp{2}).(pdtyp{3});
-                        swd = predata.(dtype).phV; % record new, precise phV from mineos
-                    case 'HV'
-                        swk = predata.(dtype).HVr; % record existing phV from kernels
-                        predata.(dtype).HVr = SW.(pdtyp{2}).(pdtyp{3});
-                        swd = predata.(dtype).HVr; % record new, precise phV from Tanimoto script
-                end
-                
-                if par.inv.verbose
-                    fprintf('   %s RMS diff is %.4f\n',par.inv.datatypes{id},rms(swk-swd)); % RMS difference
-                end
-            end
+            [ predata,HVK_new ] = b3_FORWARD_MODEL_SW_precise( model1,par,predata,ID );
 		else 
 			Ktry = [];
-		end
+        end
     end % only redo data if model has changed 
 
-%      plot_TRUvsPRE_old(trudata,predata)]
+%      plot_TRUvsPRE_old(TD.Value,predata)]
 
     % continue if any Sp or PS inhomogeneous or nan or weird output
     if ifforwardfail(predata,par)
@@ -424,12 +317,12 @@ try
 %% =========================  CALCULATE MISFIT  ===========================
     
     % SW weights, if applicable 
-    [ SWwt ] = make_SW_weight( par,Kbase,trudata );
+    [ SWwt ] = make_SW_weight( par,Kbase,TD.Value );
     
-    [ misfit1 ] = b4_CALC_MISFIT( trudata,predata,par,0,SWwt ); % misfit has structures of summed errors
+    [ misfit1 ] = b4_CALC_MISFIT( TD.Value,predata,par,0,SWwt ); % misfit has structures of summed errors
 
 %% =======================  CALCULATE LIKELIHOOD  =========================
-    [ log_likelihood1,misfit1 ] = b5_CALC_LIKELIHOOD( misfit1,trudata,model1.datahparm,par);
+    [ log_likelihood1,misfit1 ] = b5_CALC_LIKELIHOOD( misfit1,TD.Value,model1.datahparm,par);
     
 %     fprintf('MISFITS: Sp %5.2e  Ps %5.2e  SW %5.2e\n',misfit.SpRF,misfit.PsRF,misfit.SW)
 %     fprintf('CHI2S:   Sp %5.2e  Ps %5.2e  SW %5.2e\n',misfit.chi2_sp,misfit.chi2_ps,misfit.chi2_SW)
@@ -444,7 +337,7 @@ try
     
     % ======== PLOT ========  if accept
     if ifaccept && par.inv.verbose && fail_chain==0
-        plot_TRUvsPRE( trudata,predata);
+        plot_TRUvsPRE( TD.Value,predata);
         if strcmp(projname,'SYNTHETICS')
             plot_MOD_TRUEvsTRIAL( TRUEmodel, model1 );
         end
@@ -467,17 +360,7 @@ try
         
     %% UPDATE KERNEL if needed 
         if newK==true
-            Kbase.modelk = model;
-            for id = 1:length(par.inv.datatypes)
-                dtype = par.inv.datatypes{id};pdtyp = parse_dtype(dtype); 
-                if ~strcmp(pdtyp{1},'SW'), continue; end
-                if strcmp(pdtyp{2},'HV')
-                    K =  HVK_new;   
-                else
-                    K = run_kernels(trudata.(dtype).periods,pdtyp{2},pdtyp{3},ID,1,0,par.inv.verbose);
-                end
-                Kbase = populate_Kbase( Kbase,dtype,predata.(dtype).(pdtyp{3}),[],{K} );
-            end
+            [Kbase,predata] = b7_KERNEL_RESET(model,Kbase,predata,ID,par,0,HVK_new);                        
             nchain = 0;
         end
     
@@ -501,18 +384,32 @@ try
     %% <<< testing
     
     
-%% =========  redo kernel at end of burn in  =======
+%% =========  reset kernel at end of burn in or after too many iter =======
+    resetK = false;
     if newK == false && ii == par.inv.burnin
-            fprintf('\n RECALCULATING KERNEL - end of burn in\n');
-            [Kbase] = make_allkernels(model,Kbase,trudata,ID,par);
-            nchain = 0;
+            fprintf('\n RECALCULATING %s KERNEL - end of burn in\n',chainstr);
+            resetK = true;
     end
+    if newK == false && nchain > par.inv.maxnkchain
+            fprintf('\n RECALCULATING %s KERNEL - chain too long\n',chainstr);
+            resetK = true;
+    end
+    if resetK
+        % reset the kernels using the current model
+        [Kbase,predata] = b7_KERNEL_RESET(model,Kbase,predata,ID,par,1);
+        % need to also reset likelihood and misfit to the new, precise data
+        % (likelihood may have been artificially high due to kernel forward
+        % calc. approximation - if so, need to undo this, or chain will get
+        % stuck once we reset kernels).
+        [log_likelihood,misfit] = b8_LIKELIHOOD_RESET(par,predata,TD.Value,Kbase,model.datahparm);
+        nchain = 0;
+    end
+
 
         %% >>> testing
 %         accdec(ii) = struct('iter',ii,'logL_current',log_likelihood,'logL_proposed',log_likelihood1,'ifacc',ifaccept,'temp',temp,'preF',p_bd*ifpass,'newk',newK);
         %% <<< testing
-    
-    
+ 
 catch
     if par.inv.verbose, fprintf('  --SOME ERROR--\n'); end
     fail_chain = fail_chain+1;
@@ -525,10 +422,9 @@ end % on the fail_chain while...
 fprintf('\n =========== ENDING ITERATIONS %s ===========\n',chainstr)
 % save([resdir,'/chainout/',chainstr],'model0','misfits','allmodels')
 
-model0_perchain{iii} = model0;
 misfits_perchain{iii} = misfits;
 allmodels_perchain{iii} = allmodels;
-if isfield(trudata,'SW_Ray')
+if isfield(TD.Value,'SW_Ray')
     SWs_perchain{iii} = preSW;	
 end
 
