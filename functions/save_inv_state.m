6bef6c4d4a3deae22df0325ad5097fb8baf2b9e9
eilonzach <eilon@ucsb.edu>
2018-08-08 12:38:39 -0700

    added function to save inversion state periodically within the inversion - for recovery if everything crashes

---
+++ functions/save_inv_state.m
@@ -0,0 +1,10 @@
+function save_inv_state(resdir,chainstr,allmodels,misfits)
+% save_inv_state(resdir,chainstr,allmodels,misfits)
+%   function to save the state of the inversion into actual .mat files in
+%   case the inversion crashes out; we can recover the chains to some
+%   extent
+    
+    save([resdir,'/',chainstr,'_invState'],allmodels,misfits)
+
+end
+
