4cbe5f6b8a7605728ae529bd72f56244ad839464
eilonzach <eilon@ucsb.edu>
2018-08-15 09:27:58 -0700

    fixed typo in save inv state script

--- functions/save_inv_state.m
+++ functions/save_inv_state.m
@@ -1,10 +1,10 @@
-function save_inv_state(resdir,chainstr,allmodels,misfits)
+function save_inv_state(resdir,chainstr,allmodels,misfits) %#ok<INUSD>
 % save_inv_state(resdir,chainstr,allmodels,misfits)
 %   function to save the state of the inversion into actual .mat files in
 %   case the inversion crashes out; we can recover the chains to some
 %   extent
     
-    save([resdir,'/',chainstr,'_invState'],allmodels,misfits)
+    save([resdir,'/',chainstr,'_invState'],'allmodels','misfits')
 
 end
 
