0927e15a27de81f95bad5c59f772245d15c0e5df
Zach Eilon <zeilon@ldeo.columbia.edu>
2019-11-26 02:21:12 -1000

    continuting to update re HK

--- functions/get_sigma_min_prior.m
+++ functions/get_sigma_min_prior.m
@@ -21,10 +21,25 @@ if strcmp(pdt{1},'BW')
         sigma_min   = hypmin.(pdt{1}).(pdt{2}).def;
         sigma_prior = hyprior.(pdt{1}).(pdt{2}).def;
     end
+elseif strcmp(pdt{1},'RF')
+    if strcmp(pdt{4},'lo') % lo takes precedence
+        sigma_min   = hypmin.(pdt{1}).(pdt{2}).(pdt{4});
+        sigma_prior = hyprior.(pdt{1}).(pdt{2}).(pdt{4});
+    elseif strcmp(pdt{3},{'cms','ccp'}) % cms or ccp next
+        sigma_min   = hypmin.(pdt{1}).(pdt{2}).(pdt{3});
+        sigma_prior = hyprior.(pdt{1}).(pdt{2}).(pdt{3});
+    else % default
+        sigma_min   = hypmin.(pdt{1}).(pdt{2}).def;
+        sigma_prior = hyprior.(pdt{1}).(pdt{2}).def;
+    end
 elseif strcmp(pdt{1},'SW')
     % default only, for now (Rayleigh, phV)
     sigma_min   = hypmin.(pdt{1}).(pdt{2}).(pdt{3});
     sigma_prior = hyprior.(pdt{1}).(pdt{2}).(pdt{3});
+elseif strcmp(pdt{1},'HKstack')
+    % default only, (P)
+    sigma_min   = hypmin.(pdt{1}).(pdt{2});
+    sigma_prior = hyprior.(pdt{1}).(pdt{2});
 end
         
 
